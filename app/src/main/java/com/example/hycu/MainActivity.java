de0b0d91c5297afa74596689038694c159c487af
임기범 <ljy0932@nate.com>
2023-03-06 18:29:55 +0900

    2주차

--- app/src/main/java/com/example/hycu/MainActivity.java
+++ app/src/main/java/com/example/hycu/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.hycu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    protected Button toastBtl;
+    protected Toast toast;
+    @SuppressLint({"MissingInflatedId", "RestrictedApi"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("HYCU");
+        getSupportActionBar().setIcon(R.drawable.img_managemodify);
+
+        toastBtl = findViewById(R.id.toastBt);
+        toastBtl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toast = Toast.makeText(MainActivity.this, getString(R.string.strBtn), Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
     }
 }
